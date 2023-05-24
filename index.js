d5dcc95d2d0aa873e6b65ade76a9367b5b0a18af
me <macbook@dlaptop.local>
2023-05-24 16:25:49 +0300

    38: callbacks

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// 37
-// rest parameters = represents 
-// an indefinite number of parameters
-// (packs arguments into array)
+// 38
+// callback = a function passed as an argument to another function.
+// ensures that a function is not going to run before task completed
+// helps us develop asynchronous code
+// (When one function has to wait for another function)
 
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-let e = 5;
+sum(2, 3, displayConsole);
+sum(2, 3, displayDOM);
 
-console.log(sum(a,b,c,d,e,c));
-
-function sum(...numbers){
-	
-	let total = 0;
-
-	for (let number of numbers){
-		console.log("number: " + number);
-		total += number;
-	}
-	return total;
+function sum(x, y, callBack){
+	let result = x + y;
+	callBack(result);
 }
+
+function displayConsole(output){
+	console.log(output);
+}
+
+function displayDOM(output){
+	document.getElementById("myLabel").innerHTML = output;
+}
+
