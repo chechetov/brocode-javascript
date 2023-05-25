1ef40be8fdf2e5a1fc052e18238921494ffcafc6
me <macbook@dlaptop.local>
2023-05-25 22:22:18 +0300

    43: sort an array of numbers

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ let grades = [100, 50, 90, 60, 80, 70];
 grades = grades.sort(descendingSort);
 grades.forEach(print);
 
+grades = grades.sort(ascendingSort);
+grades.forEach(print);
+
 
 function descendingSort(x, y){
 	return y - x;
+}
+
+function ascendingSort(x, y){
+	return x - y;
 }
 
 function print(element){
