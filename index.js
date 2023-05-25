55ed32ec75d8762c8e61bd776d33763897543aa2
me <macbook@dlaptop.local>
2023-05-25 22:18:08 +0300

    43: sort an array of numbers

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// 41
-// array.filter() - creates a new array with all elements
-// that pass the test provided by function
+// 43
+// sort an array of numbers
 
-let ages = [18, 16, 21, 17, 19, 90];
-let adults = ages.filter(checkAge);
+let grades = [100, 50, 90, 60, 80, 70];
 
-adults.forEach(print);
+grades = grades.sort(descendingSort);
+grades.forEach(print);
 
-function checkAge(element){
-	return element >= 18;
+
+function descendingSort(x, y){
+	return y - x;
 }
 
 function print(element){
