47948eca6618e43d7b599c495dd3a8b9530d100c
me <macbook@dlaptop.local>
2023-05-25 15:37:09 +0300

    39: forEach()

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// 38
-// callback = a function passed as an argument to another function.
-// ensures that a function is not going to run before task completed
-// helps us develop asynchronous code
-// (When one function has to wait for another function)
+// 39
+// array.forEach() = execures a provided callback function
+// once for each array element
 
-sum(2, 3, displayConsole);
-sum(2, 3, displayDOM);
+let students = ["spongebob", "patrick", "squidward"];
+students.forEach(capitalize);
 
-function sum(x, y, callBack){
-	let result = x + y;
-	callBack(result);
+
+function capitalize(element, index, array){
+	array[index] = element[0].toUpperCase() + element.substring(1);
 }
 
-function displayConsole(output){
-	console.log(output);
-}
-
-function displayDOM(output){
-	document.getElementById("myLabel").innerHTML = output;
-}
-
+console.log(...students);
